38a4639d9e4e26e6229b41de6215f6e3ad4612dc
sagarhani <sagarhani33@gmail.com>
2021-04-19 21:01:30 +0530

    ✨ Progress bar for page loading state

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 import "../styles/globals.css";
 import { Provider } from "next-auth/client";
 import { NavBar } from "../components/NavBar";
+import NProgress from "nprogress"; //nprogress module
+import "nprogress/nprogress.css"; //styles of nprogress
+import Router from "next/router";
+NProgress.configure({ showSpinner: false });
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
