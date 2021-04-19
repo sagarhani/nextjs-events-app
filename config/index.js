58f7d55a15ac0acbb137e4943bac2b0afd8e7866
sagarhani <sagarhani33@gmail.com>
2021-04-19 18:19:11 +0530

    🐛 Prod server url

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://production-url.com";
+  : "https://nextjs-events-app-plum.vercel.app";
